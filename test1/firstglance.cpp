8ae90fe3e225a9d50339805fe64894acd1f30193
Grizzzotto <grizzzotto@gmail.com>
2025-10-25 01:34:04 +0200

    booooo math equations

--- test1/firstglance.cpp
+++ test1/firstglance.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    double sales = 95000;
-    const double state_tax = 0.04;
-    const double county_tax = 0.02;
-    double clean_summ = sales*(1-state_tax)*(1-county_tax);
-    cout << clean_summ;
+    
+    cout << "Enter values for x and y: ";
+    double x = 0;
+    double y = 0;
+    cin >> x >> y;
+
+    double z = pow(x, y);
+    cout << "The value is: " << z << endl;
+
+
     return 0;
 }
